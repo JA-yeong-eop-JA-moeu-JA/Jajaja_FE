b67bc8601b1a104722171e4927791134f2b8365d
witty._0 <mercury0502@dgu.ac.kr>
2025-07-03 02:49:15 +0900

    [Setting/#1] font, style 세팅
    - issue template 변경
    - title 및 파비콘 변경
    - 필요없는 파일 삭제
    - logo svg 추가
    - tailwind color 세팅
    - tailwind font 세팅
    - svgr, axios, react-dom, tanstack/react-query 라이브러리 설치

--- .github/ISSUE_TEMPLATE/📃-docs.md
+++ .github/ISSUE_TEMPLATE/📃-docs.md
@@ -1,18 +1,21 @@
 ---
-name: "📃 Docs"
-about: "Documentation writing and editing (README.md, etc.) Template "
-title: "📃 [Docs] "
-labels: 📃 Docs
+name: '📃 Chore'
+about: 'Documentation writing and editing (README.md, etc.) Template '
+title: '📃 [Chore] '
+labels: 📃 Chore
 assignees: ''
-
 ---
 
 ## 🛠️ Issue Description
-[//]: # (해당 이슈에 대한 설명을 작성해주세요.)
+
+[//]: # '해당 이슈에 대한 설명을 작성해주세요.'
 
 ## 💭 Documents and objectives
-[//]: # (문서의 종류가 무엇인지, 목적이 무엇인지 작성해주세요.)
+
+[//]: # '작업의 종류가 무엇인지, 목적이 무엇인지 작성해주세요.'
 
 ### 📝 check-lists
-[//]: # (업무 체크리스트를 작성해주세요.)
+
+[//]: # '업무 체크리스트를 작성해주세요.'
+
 - [ ] Task1
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import './App.css';
+import Logo from '@/assets/logo.svg?react';
 
 function App() {
-  return <div className="text-2xl text-red-600">hi</div>;
+  return (
+    <>
+      <Logo />
+      <div className="bg-green text-white">Green</div>
+      <div className="bg-green-hover">Hover</div>
+      <div className="text-error-3">에러 텍스트</div>
+      <div className="bg-black-0 text-black-5">검정 계열</div>
+      <p className="font-pretendard text-xl">Hello 안녕하세요! Pretendard가 보이시나요?</p>
+      <p className="text-title-semibold">오늘은 정말 완벽한 날씨입니다. Today is a perfectly clear day.</p>
+      <p className="text-title-medium">오늘은 정말 완벽한 날씨입니다. Today is a perfectly clear day.</p>
+      <p className="text-subtitle-semibold">설문조사를 완료하셨나요? Have you completed the survey?</p>
+      <p className="text-subtitle-medium">설문조사를 완료하셨나요? Have you completed the survey?</p>
+      <p className="text-body-semibold">이 문장은 본문 스타일 테스트를 위한 예시입니다. This sentence is a sample for body text styling.</p>
+      <p className="text-body-medium">이 문장은 본문 스타일 테스트를 위한 예시입니다. This sentence is a sample for body text styling.</p>
+      <p className="text-body-regular">이 문장은 본문 스타일 테스트를 위한 예시입니다. This sentence is a sample for body text styling.</p>
+      <p className="text-small-medium">※ 모든 항목을 정확히 기입해주세요. Please fill out all fields accurately.</p>
+      <p className="text-small-regular">※ 모든 항목을 정확히 기입해주세요. Please fill out all fields accurately.</p>
+      <p className="text-tiny-medium">ⓘ 저장하지 않으면 변경사항이 사라집니다. Changes will be lost if not saved.</p>
+    </>
+  );
 }
 
 export default App;
