8934765f6b2d7363c5022e2b1bf489008d88f76c
yooncandooit <dotsi0@dgu.ac.kr>
2025-07-05 01:35:13 +0900

    [Fix/#10] 체크박스 디자인 스타일 수정

--- src/components/common/checkbox.tsx
+++ src/components/common/checkbox.tsx
@@ -1,6 +1,9 @@
 import type { TCheckboxType } from '@/stores/checkboxStore';
 import { useCheckboxStore } from '@/stores/checkboxStore';
 
+import CheckboxBlank from '@/assets/icons/checkboxBlank.svg?react';
+import CheckboxFill from '@/assets/icons/checkboxFill.svg?react';
+
 interface ICheckboxProps {
   type: TCheckboxType;
 }
@@ -14,11 +17,8 @@ export default function Checkbox({ type }: ICheckboxProps) {
       onClick={() => toggle(type)}
       className={`
         w-12 h-12 p-1
-        border-2
         flex items-center justify-center
         transition-colors duration-200
-        ${isChecked ? 'bg-[#96DB00] border-[#96DB00]' : 'bg-white border-[#96DB00]'}
-        rounded
       `}
       style={{
         width: '48px',
@@ -26,11 +26,7 @@ export default function Checkbox({ type }: ICheckboxProps) {
         padding: '4px',
       }}
     >
-      {isChecked && (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
-          <polyline points="20 6 9 17 4 12" />
-        </svg>
-      )}
+      {isChecked ? <CheckboxFill /> : <CheckboxBlank />}
     </button>
   );
 }
