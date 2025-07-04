5fdeae97acfb36f1d40ca084747c45c88d59c4f6
yooncandooit <dotsi0@dgu.ac.kr>
2025-07-05 00:44:50 +0900

    [Fix/#10]: lint 오류 수정

--- src/stores/checkboxStore.ts
+++ src/stores/checkboxStore.ts
@@ -37,41 +37,35 @@ export const useCheckboxStore = create<ICheckboxStore>((set, get) => ({
     const current = get().checkedItems[type];
     const newCheckedItems = { ...get().checkedItems, [type]: !current };
 
-    // agree-all 동작 처리
     if (type === 'agree-all') {
       newCheckedItems['terms-service'] = !current;
       newCheckedItems['terms-privacy'] = !current;
       newCheckedItems['terms-age'] = !current;
       newCheckedItems['terms-financial'] = !current;
       newCheckedItems['terms-marketing'] = !current;
-    } else if (
-      type === 'terms-service' ||
-      type === 'terms-privacy' ||
-      type === 'terms-age' ||
-      type === 'terms-financial' ||
-      type === 'terms-marketing'
-    ) {
+    } else if (type === 'terms-service' || type === 'terms-privacy' || type === 'terms-age' || type === 'terms-financial' || type === 'terms-marketing') {
       const allChecked =
         newCheckedItems['terms-service'] &&
         newCheckedItems['terms-privacy'] &&
         newCheckedItems['terms-age'] &&
         newCheckedItems['terms-financial'] &&
         newCheckedItems['terms-marketing'];
-
       newCheckedItems['agree-all'] = allChecked;
     }
 
     set({ checkedItems: newCheckedItems });
   },
 
-  setChecked: (type, value) => {
+  setChecked: (type, value) =>
     set((state) => ({
       checkedItems: {
         ...state.checkedItems,
         [type]: value,
       },
-    }));
-  },
+    })),
 
-  reset: () => set(() => initialCheckedItems),
+  reset: () =>
+    set(() => ({
+      checkedItems: initialCheckedItems,
+    })),
 }));
