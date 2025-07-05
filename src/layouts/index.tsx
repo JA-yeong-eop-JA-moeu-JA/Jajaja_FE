73318b4d4fe0bf3044d146bf424c9908596aedc5
arumicube <aryum2003@gmail.com>
2025-07-06 00:54:15 +0900

    충돌 해결

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,17 @@
 import { useEffect } from 'react';
-import { Outlet,useLocation } from 'react-router-dom';
-import BottomBar from '../components/BottomBar';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import { useModalStore } from '@/stores/modalStore';
 
 import ModalProvider from '@/components/common/modal';
 
+import BottomBar from '../components/BottomBar';
+
 export default function Layout() {
   const { isModalOpen } = useModalStore();
-  
+
   const path = useLocation().pathname;
-  const showBottomBar = !isModalOpen && ['/', '/payment'].includes(path); 
+  const showBottomBar = !isModalOpen && ['/', '/payment'].includes(path);
 
   useEffect(() => {
     if (isModalOpen) {
@@ -22,11 +23,11 @@ export default function Layout() {
   return (
     <ModalProvider>
       <div className="min-h-screen flex flex-col">
-      <main className="flex-1 overflow-y-auto px-1">
-        <Outlet />
-      </main>
-      {showBottomBar && <BottomBar />}
-    </div>
+        <main className="flex-1 overflow-y-auto px-1">
+          <Outlet />
+        </main>
+        {showBottomBar && <BottomBar />}
+      </div>
     </ModalProvider>
   );
 }
