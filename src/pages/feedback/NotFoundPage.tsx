c32061738d4ad52fc13cbb9dcef231f5bee9672c
arumicube <aryum2003@gmail.com>
2025-07-08 00:14:45 +0900

    fix: review 사항 수정 완료

--- src/pages/feedback/NotFoundPage.tsx
+++ src/pages/feedback/NotFoundPage.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/common/button/button';
 import PageHeader from '@/components/head_bottom/PageHeader';
 
+import errorIcon from '@/assets/error.svg';
+
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
@@ -8,7 +10,7 @@ export default function NotFoundPage() {
 
       {/* 본문 */}
       <div className="flex flex-col flex-1 items-center justify-center text-center px-4">
-        <img src="/src/assets/error.svg" alt="에러 아이콘" className="w-[150px] h-[150px] mb-6" />
+        <img src={errorIcon} alt="에러 아이콘" className="w-[150px] h-[150px] mb-6" />
         <p className="text-base font-semibold">페이지를 찾을 수 없습니다.</p>
         <p className="text-sm text-gray-500 mt-2">잠시 후 다시 시도해주세요.</p>
       </div>
--- src/pages/feedback/exchangeOk.tsx
+++ src/pages/feedback/exchangeOk.tsx
@@ -1,14 +1,26 @@
+import { SelectButton } from '@/components/common/button/selectButton';
 import FeedbackPage from '@/components/common/FeedbackPage';
 
 export default function ExchangeCompletePage() {
   return (
-    <FeedbackPage
-      iconSrc="/src/assets/exchange.svg"
-      title="교환 신청이 접수되었습니다."
-      leftButtonText="교환 내역"
-      onLeftClick={() => (window.location.href = '/exchange/history')}
-      rightButtonText="홈으로"
-      onRightClick={() => (window.location.href = '/')}
-    />
+    <div>
+      <FeedbackPage iconSrc="/src/assets/exchange.svg" title="교환 신청이 접수되었습니다." />
+
+      <div className="w-full fixed bottom-0 left-0 right-0 z-10 pb-2">
+        <SelectButton
+          kind="select-bottom"
+          leftText="교환 내역"
+          rightText="홈으로"
+          leftVariant="left-outline"
+          rightVariant="right-orange"
+          onLeftClick={() => {
+            window.location.href = '/exchange/history';
+          }}
+          onRightClick={() => {
+            window.location.href = '/';
+          }}
+        />
+      </div>
+    </div>
   );
 }
