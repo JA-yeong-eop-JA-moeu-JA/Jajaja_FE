b0453fd321b2ef89af14210740c9f3e33fa47df5
witty._0 <mercury0502@dgu.ac.kr>
2025-07-09 05:53:58 +0900

    [Feat/#32] 검색창 화면 구현
    - 라이브러리 추가
    - Input 컴포넌트에 이벤트 추가
    - 메뉴 컴포넌트 구현
    - 태그 컴포넌트 구현
    - constants 추가
    - 검색창 UI 구현 및 간단한 로직 추가
    - 라우트 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -26,6 +26,7 @@ export default function Home() {
         <Header showSearch={false} />
       </header>
       <header className="w-full px-4 pb-2.5">
+        <Header />
         <SearchInput value={''} onFocus={() => navigate('/search')} />
       </header>
 
