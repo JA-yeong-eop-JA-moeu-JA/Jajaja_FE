a9ae08968ec80099dc9bdfb54a623b944a415189
arumicube <aryum2003@gmail.com>
2025-07-08 23:03:23 +0900

    fix: 주석수정

--- src/components/head_bottom/ProductHeader.tsx
+++ src/components/head_bottom/ProductHeader.tsx
@@ -9,15 +9,12 @@ export default function ProductHeader() {
 
   return (
     <header className="w-full max-w-sm h-[56px] px-0 flex items-center justify-between bg-white">
-      {/* 왼쪽: 뒤로가기 */}
       <button onClick={() => navigate(-1)} className="p-1">
         <img src={backIcon} alt="뒤로가기" className="w-5 h-5" />
       </button>
 
-      {/* 가운데: 여백 (자동으로 공간 확보) */}
       <div className="flex-1" />
 
-      {/* 오른쪽: 검색 + 장바구니 */}
       <div className="flex items-center gap-6">
         <img src={searchIcon} alt="검색" className="w-6" />
         <img src={cartIcon} alt="장바구니" className="w-6" />
