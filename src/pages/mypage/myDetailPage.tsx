02dca30cec8743d198686dbb49e34d9a45170fa5
SeungyeonHa <0727ha@naver.com>
2025-07-10 02:18:01 +0900

    [Feat/#44] 내 정보 관리 UI 구현 - 내 정보 관리 페이지 구현 - 마이페이지 to 내 정보 관리 버튼 연결 - 내 정보 관리 버튼 to 페이지들 연결 - 라우트 추가 및 변경 - svg 추가

---
+++ src/pages/mypage/myDetailPage.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+import PageHeader from '@/components/head_bottom/PageHeader';
+
+import Address from '@/assets/myPage/address.svg?react';
+import Profile from '@/assets/myPage/profile.svg?react';
+
+export default function MyDetailPage() {
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-screen bg-black-0">
+      <PageHeader title="내 정보 관리" />
+      <div className="flex flex-col items-center justify-center px-4 text-subtitle-medium bg-white">
+        <button className="w-full flex flex-col items-center justify-center gap-1 pb-6 border-b border-black-1" onClick={() => navigate('/myPage/me/profile')}>
+          <Profile className="w-30 h-30" />
+          <p>프로필 수정</p>
+        </button>
+        <button className="w-full flex flex-col items-center justify-center gap-2 pb-6 pt-7" onClick={() => navigate('/myPage/me/address')}>
+          <Address className="w-30 h-30" />
+          <p>배송지 관리</p>
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,10 @@ import NotFound from '@/pages/feedback/NotFoundPage';
 import ReturnOK from '@/pages/feedback/returnOk';
 import Home from '@/pages/home';
 import Login from '@/pages/login';
-import MyPage from '@/pages/myPage';
+import Address from '@/pages/mypage/address';
+import MyDetailPage from '@/pages/mypage/myDetailPage';
+import MyPage from '@/pages/mypage/myPage';
+import Profile from '@/pages/mypage/profile';
 import Notice from '@/pages/notice';
 import OnBoarding from '@/pages/onBoarding';
 import Payment from '@/pages/payment';
@@ -20,6 +23,9 @@ export const router = createBrowserRouter([
       { path: 'home', element: <Home /> },
       { path: 'login', element: <Login /> },
       { path: 'myPage', element: <MyPage /> },
+      { path: 'myPage/me', element: <MyDetailPage /> },
+      { path: 'myPage/me/profile', element: <Profile /> },
+      { path: 'myPage/me/address', element: <Address /> },
       { path: 'notice', element: <Notice /> },
       { path: 'payment', element: <Payment /> },
       { path: 'exchange/complete', element: <ExchangeOK /> },
