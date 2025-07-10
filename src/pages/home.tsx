179968f88504694237307833d0da63f8b430a86f
witty._0 <mercury0502@dgu.ac.kr>
2025-07-10 15:14:11 +0900

    fix: header padding 조절

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -22,8 +22,10 @@ export default function Home() {
   }, [openModal]);
   return (
     <div className="pb-15">
+      <header className="px-3">
+        <Header showSearch={false} />
+      </header>
       <header className="w-full px-4 pb-2.5">
-        <Header />
         <SearchInput value={''} onFocus={() => navigate('/search')} />
       </header>
 
