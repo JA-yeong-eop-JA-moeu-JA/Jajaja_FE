f06269e693dc1ae979be5a8cf81e14ffd3ded503
witty._0 <mercury0502@dgu.ac.kr>
2025-07-10 15:09:47 +0900

    fix: padding 고려해서 밖으로 뺌

--- src/components/modal/homeModal.tsx
+++ src/components/modal/homeModal.tsx
@@ -34,11 +34,10 @@ export default function HomeModal() {
           </div>
           <Point />
         </div>
-        <Button kind="basic" variant="solid-orange" className="w-full override-button-padding" onClick={() => closeModal()}>
-          기간 한정 포인트 확인하기
-        </Button>
       </div>
-
+      <Button kind="basic" variant="solid-orange" className="w-full" onClick={() => closeModal()}>
+        기간 한정 포인트 확인하기
+      </Button>
       <div className="flex items-center px-4 py-3">
         <BaseCheckbox message="오늘 하루 보지 않기" checked={checked} onClick={() => setChecked(!checked)} />
       </div>
