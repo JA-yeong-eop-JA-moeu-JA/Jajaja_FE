fce601c83145b1f3be9f87cb6b9f167dd147bab9
SeungyeonHa <0727ha@naver.com>
2025-07-12 00:16:39 +0900

    [Feat/#63] 쿠폰 페이지 UI 구현 - 쿠폰 컴포넌트 옵션 수정 - 쿠폰 컴포넌트 UI 수정 - 마이페이지 내 쿠폰 페이지 구현 - 라우트 수정

--- src/pages/mypage/coupons.tsx
+++ src/pages/mypage/coupons.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import type { ICoupon } from '@/constants/coupon/coupons';
 
@@ -8,9 +7,8 @@ import { CouponStore } from '@/stores/couponStore';
 import CouponCard from '@/components/coupon/CouponCard';
 import PageHeader from '@/components/head_bottom/PageHeader';
 
-export default function CouponsPage() {
-  const navigate = useNavigate();
-  const { ownedCoupons, selectedCouponId, selectCoupon, issueSignupCoupons } = CouponStore();
+export default function Coupons() {
+  const { ownedCoupons, issueSignupCoupons } = CouponStore();
 
   useEffect(() => {
     if (ownedCoupons.length === 0) {
@@ -18,20 +16,13 @@ export default function CouponsPage() {
     }
   }, []);
 
-  const handleSelect = (id: number) => {
-    selectCoupon(id);
-    setTimeout(() => {
-      navigate(-1);
-    }, 200);
-  };
-
   return (
     <>
       <PageHeader title="쿠폰" />
       <div className="w-full h-screen bg-white">
         <div className="flex flex-col gap-3 px-3">
           {ownedCoupons.map((coupon: ICoupon) => (
-            <CouponCard key={coupon.id} coupon={coupon} isSelected={selectedCouponId === coupon.id} onClick={() => handleSelect(coupon.id)} />
+            <CouponCard key={coupon.id} coupon={coupon} />
           ))}
         </div>
       </div>
