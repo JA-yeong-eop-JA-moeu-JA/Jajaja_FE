5fae518b69e0ae911737a4419a240aeaf1428a50
SeungyeonHa <0727ha@naver.com>
2025-07-14 01:41:12 +0900

    fix: lint 오류 수정

--- src/mocks/profileData.ts
+++ src/mocks/profileData.ts
@@ -1,12 +1,12 @@
-export interface ProfileData {
-    name: string;
-    phoneNumber: string;
-    email: string;
-    profileImage?: string;
+export interface IProfileData {
+  nickname: string;
+  phoneNumber: string;
+  email: string;
+  profileImage?: string;
 }
 
-export const profileData: ProfileData = {
-    name: "이한비",
-    phoneNumber: "010-3424-2342",
-    email: "rum080d8d@gmail.com",
-}
+export const profileData: IProfileData = {
+  nickname: '이한비',
+  phoneNumber: '010-3424-2342',
+  email: 'rum080d8d@gmail.com',
+};
