0bebafb7d136ed16f5508fb3280461e46f46c3bb
witty._0 <mercury0502@dgu.ac.kr>
2025-07-31 17:09:22 +0900

    feat: axios 환경세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr({ include: '**/*.svg?react' })],
@@ -9,5 +13,12 @@ export default defineConfig({
     alias: {
       '@': '/src',
     },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+    },
+    port: 3000,
   },
 });
