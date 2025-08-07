584925dfec7ff66acae137f6d1b3a06232ec4453
SeungyeonHa <0727ha@naver.com>
2025-08-07 21:12:17 +0900

    [Feat/#112] 쿠폰 api 연동 - get /api/coupons 연동(axiosInstance, type, 훅 작성) - CouponCard, Coupons 데이터 정보로 업데이트 - profile.tsx에서 기본 이미지 파일 삭제 - 결제 창의 coupon 페이지 임시 주석 처리 - date-fns, react-intersection-observer 추가

---
+++ src/types/TPage.ts
@@ -0,0 +1,10 @@
+export type TPage = {
+  size: number;
+  totalElements: number;
+  currentElements: number;
+  totalPages: number;
+  currentPage: number;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+  isLast: boolean;
+};
