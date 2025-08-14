d9518673b845dc58bcca85da537f67d7d28261b2
SeungyeonHa <0727ha@naver.com>
2025-08-14 15:18:52 +0900

    [Bug/#187] /me 무한 재호출 버그 수정

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -14,9 +14,9 @@ export default function ProtectedLayout() {
           <Loading />
         </div>
       ) : !isError && isLoggedIn ? (
-        <div className="h-screen" />
-      ) : (
         <Outlet />
+      ) : (
+        <div className="h-screen" />
       )}
     </Layout>
   );
