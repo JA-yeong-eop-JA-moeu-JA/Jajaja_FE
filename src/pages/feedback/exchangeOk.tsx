e7588639eeba04961ff792d8281a51fb05c64a77
arumicube <aryum2003@gmail.com>
2025-08-16 00:28:53 +0900

    fix: exchange

--- src/pages/feedback/exchangeOk.tsx
+++ src/pages/feedback/exchangeOk.tsx
@@ -1,10 +1,12 @@
 import { SelectButton } from '@/components/common/button/selectButton';
 import FeedbackPage from '@/components/common/FeedbackPage';
 
+import exchangeIcon from '@/assets/exchange.svg';
+
 export default function ExchangeCompletePage() {
   return (
     <div>
-      <FeedbackPage iconSrc="/src/assets/exchange.svg" title="교환 신청이 접수되었습니다." />
+      <FeedbackPage iconSrc={exchangeIcon} title="교환 신청이 접수되었습니다." />
 
       <div
         className="fixed bottom-0 left-0 right-0 z-10 pb-2
