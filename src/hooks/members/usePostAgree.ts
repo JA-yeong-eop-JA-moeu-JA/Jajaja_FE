fba984149e66ac0fe666afa8a39c10d878e42ad3
witty._0 <mercury0502@dgu.ac.kr>
2025-08-18 01:09:32 +0900

    [Fix] 회원가입 성공시 업종 저장

--- src/hooks/members/usePostAgree.ts
+++ src/hooks/members/usePostAgree.ts
@@ -1,8 +1,18 @@
+import Storage from '@/utils/storage';
 import { postAgree } from '@/apis/members/members';
+
+import useCategory from '@/hooks/onBoarding/useCategory';
 
 import { useCoreMutation } from '../customQuery';
 
 export default function usePostAgree() {
-  const { mutate } = useCoreMutation(postAgree);
+  const { mutate: setCategory } = useCategory();
+  const { mutate } = useCoreMutation(postAgree, {
+    onSuccess: () => {
+      if (Storage.getCategory()) {
+        setCategory({ businessCategoryId: Number(Storage.getCategory()) });
+      }
+    },
+  });
   return { mutate };
 }
