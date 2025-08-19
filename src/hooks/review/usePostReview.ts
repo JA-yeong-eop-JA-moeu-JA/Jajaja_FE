133ca0676c0ffabd6827a7d7b44dc57a0e955c0d
SeungyeonHa <0727ha@naver.com>
2025-08-19 21:04:06 +0900

    [Fix/#250] 리뷰 작성 버그 수정 - 작성한 리뷰 UI 수정 - 이미지 업로더 훅 로직 수정(zustand 기반) - 리뷰 이미지 업로드 로직 수정 - 프로필 이미지 업로드 로직 수정 - 리뷰 삭제 낙관적 업데이트로 수정 - 클라이언트 키 노출된 코드 삭제

--- src/hooks/review/usePostReview.ts
+++ src/hooks/review/usePostReview.ts
@@ -1,15 +1,20 @@
+import { useQueryClient } from '@tanstack/react-query';
+
 import type { TPostReview, TPostReviewRequest } from '@/types/review/myReview';
+import { QUERY_KEYS } from '@/constants/querykeys/queryKeys';
 
 import { postReview } from '@/apis/review/myReview';
 
 import { useCoreMutation } from '@/hooks/customQuery';
 
 export default function usePostReview() {
+  const queryClient = useQueryClient();
   const { mutate } = useCoreMutation<TPostReview, TPostReviewRequest>(
     ({ productId, rating, content, imageKeys }) => postReview({ productId, rating, content, imageKeys }),
     {
       onSuccess: () => {
-        window.location.replace('/mypage/review');
+        queryClient.invalidateQueries({ queryKey: QUERY_KEYS.GET_REVIEWABLE });
+        queryClient.invalidateQueries({ queryKey: QUERY_KEYS.GET_MY_REVIEW });
       },
     },
   );
